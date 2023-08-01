1bd6c782b92b6fd057ad7784c1dbc0c6873c2ecf
Angela R <angelaribeiro2@hotmail.com>
2023-08-01 12:38:10 -0300

    ok

--- src/components/Carrossel.vue
+++ src/components/Carrossel.vue
@@ -1,40 +1,40 @@
 <template>
-  <v-card class="mx-auto" width="1100" height="550" density="compact">
-  <v-slide-group show-arrows  >
-    <v-slide-group-item v-for="(item, index) in items" :key="index">
-      <v-container>
-        <v-row>
-          <v-col cols="12" sm="6" md="4" lg="4">
-            <v-card  width="300" height="500" class="border" elevation="3">
-              <v-img :src="item.image"></v-img>
-              <v-card-title class="pt-3 text-center title-wrap">
-                  {{  item.title }}
-               </v-card-title>
-              <v-card-text class="text-center info-credits">
-                <p>{{ item.description }}</p>
-              </v-card-text>
-              <v-card-actions class="d-flex justify-center">
-                <v-btn
-                  append-icon="mdi-cart"
-                  variant="tonal"
-                  :color="item.color"
-                  >Comprar</v-btn
-                >
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-slide-group-item>
-  </v-slide-group>
-  </v-card>
+  <v-sheet class="mx-auto" width="1100" height="550" density="compact">
+    <v-slide-group show-arrows v-model="activeSlide" @input="resetTimer">
+      <v-slide-group-item v-for="(item, index) in items" :key="index">
+        <v-container>
+          <v-row>
+            <v-col cols="12" sm="6" md="4" lg="4">
+              <v-card width="300" height="500" class="border" elevation="3">
+                <v-img :src="item.image"></v-img>
+                <v-card-title class="pt-3 text-center title-wrap">
+                  {{ item.title }}
+                </v-card-title>
+                <v-card-text class="text-center info-credits">
+                  <p>{{ item.description }}</p>
+                </v-card-text>
+                <v-card-actions class="d-flex justify-center">
+                  <v-btn
+                    append-icon="mdi-cart"
+                    variant="tonal"
+                    :color="item.color"
+                    >Comprar</v-btn
+                  >
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-slide-group-item>
+    </v-slide-group>
+  </v-sheet>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      hideDelimiters: false,
+      activeSlide: 0,
       items: [
         {
           title: "Carregue R$110 e tenha R$220",
@@ -62,8 +62,7 @@ export default {
         },
         {
           title: "Oferta especial para empresas parceiras",
-          description:
-            "*Pague R$40 e tenha R$100 para brincar!",
+          description: "*Pague R$40 e tenha R$100 para brincar!",
           image:
             "https://stgimgs.blob.core.windows.net/promotion/7ZFQeDIsfFyxl78tgmuTy8LWQLyX8W5ZwNypBHrz.jpeg",
           color: "#42A5F5",
@@ -76,7 +75,7 @@ export default {
             "https://stgimgs.blob.core.windows.net/promotion/OwI2jPJuzIrw8tAkHBxo2S7xbMwg7jyldoEBAyGO.jpeg",
           color: "#66BB6A",
         },
-                {
+        {
           title: "Carregue R$110 e tenha R$220",
           description:
             "*R$110 em crédito e ganhe mais R$110 em bônus e mais 1000 TICKETS",
@@ -86,6 +85,26 @@ export default {
         },
       ],
     };
+  },
+  mounted() {
+    // Inicia o carrossel com avanço automático
+    this.startCarouselAutoAdvance();
+  },
+  methods: {
+    startCarouselAutoAdvance() {
+      // Define um intervalo para avançar o slide a cada 5 segundos
+      this.carouselInterval = setInterval(() => {
+        this.activeSlide = (this.activeSlide + 1) % this.items.length;
+      }, 1000); // 5000 ms (5 segundos)
+    },
+    stopCarouselAutoAdvance() {
+      // Limpa o intervalo quando o componente é destruído para evitar vazamentos de memória
+      clearInterval(this.carouselInterval);
+    },
+  },
+  beforeDestroy() {
+    // Garante que o intervalo é limpo quando o componente é destruído
+    this.stopCarouselAutoAdvance();
   },
 };
 </script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -39,6 +39,32 @@
     </section>
   </v-main>
 
+  <section id="chamada">
+    <v-container>
+      <v-row>
+        <v-col>
+          <div class="box-offers-fixed-top">
+            <h2 class="title-offers-fixed">Tickets de montão</h2>
+            <p class="text-offers">
+              Carregue R$ 110 e tenha *R$ 220 para brincar muito e mais 110
+              tickets para trocar por prêmios!
+              <br />
+              R$ 110 em créditos + R$ 110 em bônus
+            </p>
+          </div>
+          <div class="box-offers-fixed-middle">
+            <div class="money">R$</div>
+            <div class="money1">110</div>
+            <div class="money2">,00</div>
+          </div>
+
+          <div class="box-offers-fixed-bottom">
+            <v-btn color="#ffd100" class="btn-buy">Comprar</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
   <v-footer></v-footer>
 </template>
 
@@ -48,7 +74,7 @@ import Carrossel from "../components/Carrossel.vue";
 import Steps from "../components/Steps.vue";
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .background-image {
   background-image: url("https://stgimgs.blob.core.windows.net/banners/oiilVut4mCac1PTcbZUgF0xz3bIg6hUROF1UkveW.png");
   background-repeat: no-repeat;
@@ -57,7 +83,7 @@ import Steps from "../components/Steps.vue";
 svg {
   margin-top: 150px;
   -webkit-filter: drop-shadow(5px 5px 0 black) drop-shadow(-5px -5px 0 black);
-  filter: drop-shadow(0px -7px 0 #7367F0) drop-shadow(-7px 5px 0 #7367F0)
+  filter: drop-shadow(0px -7px 0 #7367f0) drop-shadow(-7px 5px 0 #7367f0);
 }
 
 .title-section {
@@ -79,5 +105,66 @@ svg {
   margin: 0 auto;
   padding: 0 28em;
   font-weight: 600;
+}
+
+.title-offers-fixed {
+  font-family: "Public Sans", sans-serif;
+  font-weight: 500;
+  font-size: 2.5em;
+  color: #fed200;
+  margin-top: 40px;
+  text-transform: uppercase;
+}
+
+.text-offers {
+  font-family: "Public Sans", sans-serif;
+  color: #000;
+  font-size: 1.1em;
+  line-height: 1.7em;
+  margin-bottom: 2em;
+}
+
+.box-offers-fixed-middle {
+  width: 77% !important;
+}
+
+.money {
+  font-family: "Public Sans", sans-serif;
+  font-size: 4em;
+  color: #ffd100;
+  font-weight: 900;
+  float: left;
+  margin-top: 11%;
+}
+
+.money1 {
+  font-family: "Public Sans", sans-serif;
+  font-weight: 900;
+  color: #ffd100;
+  font-size: 9em;
+  margin-top: 0;
+  float: left;
+}
+
+.money2 {
+  font-size: 4em;
+  font-weight: 900;
+  color: #ffd100;
+  display: block;
+  float: left;
+  margin-top: 8.5%;
+}
+
+.btn-buy {
+  font-family: "Public Sans", sans-serif;
+
+  color: #ff287b;
+  font-size: 1.1em;
+  font-weight: bold;
+  padding-left: 1.4em;
+  padding-right: 1.4em;
+  padding-top: 0.4em;
+  padding-bottom: 0.3em;
+  border-radius: 6px;
 }
 </style>
